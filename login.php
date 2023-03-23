1c3430df86f763c81f8e31133202333339197eaa
Wafula-Joseph <38218860+Wafula-Joseph@users.noreply.github.com>
2023-03-23 13:50:26 +0300

    Update login.php

--- login.php
+++ login.php
@@ -9,19 +9,15 @@
 <body>
     <?php
 if($_POST["uname"]=="admin" and $_POST["psw"]==1234){
-    //echo "Welcome! You are the admin";
+    
 
 
 
    echo "<img src=\"adminimg.png\" alt=\"image\" width=100% align=center >";
 
-   
-   //echo "<img src=\"image.jpg\" alt=\"Image\">";
-
-    //("location:admin.html");
-     exit;
+    
+    
 } else {
-//echo "Wrong username or password";
 
 echo "<img src=\"oops.png\" alt=\"image\" width=100% align-text=center >";
 }
